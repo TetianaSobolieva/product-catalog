50c1a0c543831ea6867bcd947ba8664860895f6e
Tetiana Sobolieva <tetiana.sobolieva.s@gmail.com>
2026-05-27 09:45:56 +0300

    addede hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
-import { ProductProvider } from "./context/ProductProvider"; 4
-import './index.css'
+import { ProductProvider } from "./context/ProductProvider";
+import './index.css';
+
+import "./index.css";
 
 ReactDOM.createRoot(
   document.getElementById("root")!,
 ).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ProductProvider>
         <App />
       </ProductProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
